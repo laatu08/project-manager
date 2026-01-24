511058f1f3d3a52681bd5ee880ff6af59b633d7d
laatu08 <partha2002borah@gmail.com>
2026-01-24 23:23:38 +0530

    silly mistake

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://project-manager-62vb.vercel.app/",
+  baseURL: "https://project-manager-62vb.vercel.app",
   // baseURL: "https://project-manager-1iwt.onrender.com",
   withCredentials: true,
 });
