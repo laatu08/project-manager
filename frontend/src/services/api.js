a4f01cdf87a3e638e7158ef3d31aa825254d6d7d
laatu08 <partha2002borah@gmail.com>
2026-01-24 23:51:45 +0530

    switch back to render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://project-manager-62vb.vercel.app",
-  // baseURL: "https://project-manager-1iwt.onrender.com",
+  // baseURL: "https://project-manager-62vb.vercel.app",
+  baseURL: "https://project-manager-1iwt.onrender.com",
+  // baseURL: "http://localhost:5000",
   withCredentials: true,
 });
